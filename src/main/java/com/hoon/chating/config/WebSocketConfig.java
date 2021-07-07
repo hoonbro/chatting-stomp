8214aa2a8c427e60f400a98ae42914b1efc92a10
Jihoon Byun <wlgns1588@naver.com>
2021-07-07 22:07:58 +0900

    chat init

---
+++ src/main/java/com/hoon/chating/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.hoon.chating.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.hoon.chating.handler.SocketHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer{
+	@Autowired
+	SocketHandler socketHandler;
+	
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(socketHandler, "/chating");
+	}
+}
---
+++ src/test/java/com/hoon/chating/SocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoon.chating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
